07e100109c405ac3be99e354692ec8ddb09c5115
Manuel Laug <laugmanuel@users.noreply.github.com>
2024-05-17 20:38:44 +0200

    properly prepend ProvisioningTemplate class with helper (#28)
    fixes error during settings rendering because method is not found

--- app/models/concerns/foreman_rescue/provisioning_template_extensions.rb
+++ app/models/concerns/foreman_rescue/provisioning_template_extensions.rb
@@ -2,7 +2,7 @@
 
 module ForemanRescue
   module ProvisioningTemplateExtensions
-    def self.templates_by_kind(kind)
+    def templates_by_kind(kind)
       template_kind = TemplateKind.find_by(name: kind)
       ProvisioningTemplate.where(:template_kind => template_kind).pluck(:name, :name).to_h
     end
